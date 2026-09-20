2177d3eae7e839313dd399ca5cac111da674a3e9
Marwan.Abed <Marwan.Abed@GIZA_60>
2026-09-20 13:22:29 +0300

    First CI CD

---
+++ CICDDemo.Tests/Tests/CalculatorServiesTest.cs
@@ -0,0 +1,27 @@
+﻿using CICDDemo.Services;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CICDDemo.Tests.Tests
+{
+    [TestFixture]
+    public class CalculatorServiesTest
+    {
+        [Test]
+        public void Add_TwoNumbers_ReturnsSum()
+        {
+            // Arrange
+            var calculatorService = new CalculatorService();
+            int a = 5;
+            int b = 10;
+            // Act 
+            int result = calculatorService.Add(a, b);
+            // Assert
+            Assert.That(result, Is.EqualTo(15));
+        }
+    }
+}
---
+++ CICDDemo/Services/CalculatorService.cs
@@ -0,0 +1,10 @@
+﻿namespace CICDDemo.Services
+{
+    public class CalculatorService
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
